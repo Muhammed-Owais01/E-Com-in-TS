3bbdd7d9bc8fad5f84889c41796a01483d5cffb6
Muhammed-Owais01 <workandrecovery@gmail.com>
2024-07-17 15:46:18 +0500

    Added Return Type

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -1,10 +1,11 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { NextFunction, Request, Response } from "express";
 import UserService from "../services/user";
 import RequestError from "../exceptions/requestError";
 import Cart from "../models/cart";
+import { ReturnResponse } from "../types/global";
 
 class UserController {
-    async user_signup(req: Request, res: Response, next: NextFunction) {
+    async user_signup(req: Request, res: Response, next: NextFunction): Promise<ReturnResponse> {
         const { username, password }: { username: string | undefined, password: string | undefined } = req.body;
         if (!username || !password) throw new RequestError("Invalid Request", 405);
 
@@ -13,7 +14,7 @@ class UserController {
         return res.status(200).json({ "message": "User created" });
     }
 
-    async user_login(req: Request, res: Response, next: NextFunction) {
+    async user_login(req: Request, res: Response, next: NextFunction): Promise<ReturnResponse> {
         const { username, password }: { username: string | undefined, password: string | undefined} = req.body;
         if (!username || !password) throw new RequestError("Invalid Request", 405);
 
@@ -25,7 +26,7 @@ class UserController {
         });
     }
 
-    async update_user(req: Request, res: Response, next: NextFunction) {
+    async update_user(req: Request, res: Response, next: NextFunction): Promise<ReturnResponse> {
         const id: number = parseInt(req.params.userId);
         
         const { username, password }: { username?: string, password?: string} = req.body;
@@ -35,7 +36,7 @@ class UserController {
         return res.status(200).json({ message: "User Updated" });
     }
 
-    async delete_user(req: Request, res: Response, next: NextFunction) {
+    async delete_user(req: Request, res: Response, next: NextFunction): Promise<ReturnResponse> {
         const id: number = parseInt(req.params.userId);
 
         const deletedUser = await UserService.deleteUser(id);
--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -5,12 +5,12 @@ import checkAuth from "../middleware/check-auth";
 
 const router: Router = Router();
 
-router.post('/signup', UserController.user_signup);
+router.post('/signup', asyncHandler(UserController.user_signup));
 
-router.post('/login', UserController.user_login);
+router.post('/login', asyncHandler(UserController.user_login));
 
-router.patch('/:userId', checkAuth, UserController.update_user);
+router.patch('/:userId', checkAuth, asyncHandler(UserController.update_user));
 
-router.delete('/:userId', checkAuth, UserController.delete_user);
+router.delete('/:userId', checkAuth, asyncHandler(UserController.delete_user));
 
 export default router;
